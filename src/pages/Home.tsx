f8a95e37c6981532429a0138feb9c98fd488153d
pavelkmorning <pavel.k@morning.com>
2023-11-08 09:53:52 +0100

    Zustand could be tested

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import {Counter} from "../components/counter";
+
 function Home() {
-  return <h1>Hello World</h1>;
+  return (
+      <>
+          <h1>Hello World</h1>
+            <Counter/>
+      </>
+
+  );
 }
 
 export default Home;
